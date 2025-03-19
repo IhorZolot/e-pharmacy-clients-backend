1eaaa1b2327ec883e87d1526615d492cb072abde
IhorZolot <fedonn@ukr.net>
2025-03-19 23:54:19 +0200

    add server and Database MongooseDB connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+import 'dotenv/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(
+      process.env.DB_HOST || 'mongodb://localhost:27017/mydatabase',
+      {
+        connectionFactory: (connection: Connection) => {
+          console.log('✅ Connected to MongoDB');
+          return connection;
+        },
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
